1b2cd4a6d3d0c31dc2e959b73ef40b19062b7d89
Kristian Padilla <ketpadilla05@gmail.com>
2023-11-15 19:36:36 +0800

    Part 1 Summary Test

--- notes/computations.cpp
+++ notes/computations.cpp
@@ -3,14 +3,20 @@
 #include <string>
 using namespace std;
 
+double eval(double x) {
+    return x / (.5 * x);
+} void use(double n) {
+    int v = 1 / n;
+    v = eval(v);
+    cout << v << endl;
+}
 int main() {
     // M1 Test: Focused on value of literals (E, hex, oct, dec)
     // M2 Test: Focused on output of programs (conditionals, loops, & strucutres)
     // M3 Test: Focused on output of programs (functions, arrays, & pointers)
     // M4 Test: Focused on output of programs (namespaces, strings, arrays, & pointers)
+    // Part 1 Summary Test: Covers M1-M4
 
-    return 0;
+    
 
 }
-
-
